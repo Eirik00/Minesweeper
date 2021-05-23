82a6f4e76c739ecbdf8730a436e43286cbde78d8
Eirik00 <eirik.rene@gmail.com>
2021-05-23 21:31:28 +0200

    23.05.2021
    kan nå høyre klikke for å plassere flagg, trenger å ha en funksjon. Laget tall bilder og er nesten ferdig.

--- js.js
+++ js.js
@@ -2,12 +2,17 @@ let gridPlace = document.getElementById("gamewindow");
 let gridH = document.getElementById("gHeight");
 let gridW = document.getElementById("gWidth");
 let gridBomb = document.getElementById("gbombs");
+let makeBombQ = 0;
+let usedClass = [];
+let howManyBombs = 0;
+let numbmb = 0;
+let cantplay = 0;
 
 function gameInit() {
     let gridAmmount = gridH.value*gridW.value;
     let gridAuto = "";
     for(let i=0;i<gridW.value;i++){
-        gridAuto = "170px "+gridAuto;
+        gridAuto = "36px "+gridAuto;
     }
     gridPlace.style = "grid-template-columns: "+gridAuto;
     let curColum = 1;
@@ -17,16 +22,48 @@ function gameInit() {
         nw.setAttribute("id", curLine+","+curColum);
         nw.setAttribute("class", "grid-item");
         nw.onclick = function() {
+            if(makeBombQ == 0){genBombs(this.id);}
+            if(cantplay == 1){return;}
+            makeBombQ = 1;
             let Cnumbr = this.childNodes[0];
+            this.setAttribute("class", "shownSquare");
             if(Cnumbr.style.visibility != "visible"){
                 Cnumbr.style.visibility = "visible";
                 let nw = document.createElement("img");
                 if(Cnumbr.innerHTML == "B"){
                     nw.src="img/explosion.png";
-                    Cnumbr.style.display = "none";
+                    cantplay = 1;
+                }else{
+                    nw.src="img/"+Cnumbr.innerHTML+".png";
                 }
                 this.appendChild(nw);
+                checkPlay();
+                return;
             }
+        }
+        nw.oncontextmenu = function() {
+            if(makeBombQ == 0){return false;}
+            if(cantplay == 1){return false;}
+            let Cnumbr = this.childNodes[0];
+            if(Cnumbr.style.visibility != "visible"){
+                if(howManyBombs <= 0){return false;}
+                Cnumbr.style.visibility = "visible";
+                let nw = document.createElement("img");
+                nw.src = "img/flag.png";
+                this.appendChild(nw);
+                howManyBombs--;
+                document.getElementById("leftBmbs").innerHTML = "Bombs Left: "+howManyBombs;
+            }else if(Cnumbr.style.visibility == "visible"){
+                if(this.childNodes[1].src.endsWith("flag.png")){  
+                    this.childNodes[1].remove();
+                    Cnumbr.style.visibility = "hidden";
+                    howManyBombs++;
+                    document.getElementById("leftBmbs").innerHTML = "Bombs Left: "+howManyBombs;
+                }else{
+                    return false;
+                }
+            }
+            return false;
         }
         gridPlace.appendChild(nw);
         nw = document.createElement("p");
@@ -40,13 +77,17 @@ function gameInit() {
         }
         console.log(gridW.value)
     }
-    console.log("hey");
-    
-    let bombAmmount = gridBomb.value/100;
     let totalsq = gridW.value*gridH.value;
-    let usedClass = [];
-    let numBmb = Math.floor(bombAmmount*totalsq);
+    let bombAmmount = gridBomb.value/100;
+    numBmb = Math.floor(bombAmmount*totalsq);
+    howManyBombs = numBmb;
+    document.getElementById("leftBmbs").innerHTML = "Bombs Left: "+howManyBombs;
+}
+
+function genBombs(cBmb){
+    usedClass.push(cBmb);
     let currbmb = 0;
+    
     for(let i=0;i<numBmb;i++){
         let x = 1+Math.floor(Math.random()*(gridH.value-1));
         let y = 1+Math.floor(Math.random()*(gridW.value-1));
@@ -57,19 +98,16 @@ function gameInit() {
             bd.className = "bombers";
             document.getElementById(divClass).childNodes[0].innerHTML = "B";
             usedClass.push(divClass);
-            
-            
-            let nw = document.createElement("div");
-            nw.innerHTML = x+" "+y;
-            nw.style.display = "none";
-            bd.appendChild(nw);
             currbmb++;
         }else{numBmb++;}
     }
+    usedClass.shift();
     for(let i=0;i<currbmb;i++){
-        let curVal = document.getElementsByClassName("bombers")[i].childNodes[1].innerHTML;
-        let x = curVal.split(" ")[0];
-        let y = curVal.split(" ")[1];
+        let curVal = usedClass[i];
+        console.log(document.getElementsByClassName("bombers"));
+        console.log(usedClass);
+        let x = curVal.split(",")[0];
+        let y = curVal.split(",")[1];
         console.log("x:"+x+"|y:"+y);
         
         let up = parseInt(x)-1;
@@ -151,7 +189,42 @@ function gameInit() {
             }
         }
     }
-    console.log(usedClass);
-    console.log(bombAmmount*totalsq);
 }
-document.getElementById("startGen").onclick = gameInit;
+function checkPlay(){
+    if(cantplay == 1){
+        document.getElementById("failed").style.display = "block";
+
+    }else{
+        document.getElementById("failed").style.display = "none";
+    }
+}
+function removeGrids() {
+    var gridItems = document.getElementById("gamewindow").childNodes;
+    let gridLength = gridItems.length;
+    for(let i=0;i<gridLength-1;i++){
+        gridItems[1].remove();
+    }
+}
+document.getElementById("restart").onclick = function() {
+    removeGrids();
+    cantplay = 0;
+    howManyBombs = 0;
+    gridPlace = document.getElementById("gamewindow");
+    gridH = document.getElementById("gHeight");
+    gridW = document.getElementById("gWidth");
+    gridBomb = document.getElementById("gbombs");
+    makeBombQ = 0;
+    usedClass = [];
+    howManyBombs = 0;
+    numbmb = 0;
+    cantplay = 0;
+    
+    document.getElementById("leftBmbs").innerHTML = "Bombs Left: "+howManyBombs;
+    document.getElementById("startGen").style.display = "block";
+    document.getElementById("failed").style.display = "none";
+}
+document.getElementById("startGen").onclick = function() {
+    this.style.display = "none";
+    cantplay = 0;
+    gameInit();
+};
